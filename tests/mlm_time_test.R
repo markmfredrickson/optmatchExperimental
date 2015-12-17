cf08e17f093cdd10274e66ce995767f435173e69
Josh Errickson <josh@errickson.net>
2015-12-17 11:42:14 -0500

    Making the speed test faster when running make check

--- tests/mlm_time_test.R
+++ tests/mlm_time_test.R
@@ -1,7 +1,11 @@
 rm(list=ls())
 library(optmatch)
 library(optmatchExperimental)
-n <- 4000
+
+## Leaving this small for `make check`, make it bigger if actually
+## testing speed.
+#n <- 4000
+n <- 100
 
 d <- data.frame(x=rbinom(n, 1, .4),
                 y=rbinom(n, 1, .02),
